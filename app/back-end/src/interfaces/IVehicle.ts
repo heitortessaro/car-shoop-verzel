14129ab9421525cfd5c682fa246c2b53487ecd41
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-05 21:26:22 -0300

    fix(dev-back-end): update IVehicle interface
    image input will be a string which is optional

--- app/back-end/src/interfaces/IVehicle.ts
+++ app/back-end/src/interfaces/IVehicle.ts
@@ -13,12 +13,7 @@ const VehicleZodSchema = z.object({
     .lte(2022, { message: 'Year must be smaller equal 2022' }),
   color: z.string().min(3, { message: 'Color must be 3 or more characters long' }),
   buyValue: z.number().positive({ message: 'BuyValue must be greater then 0' }),
-  image: z.any()
-    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-    .refine(
-      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported."
-    )
+  image: z.string().optional()
 });
 
 type IVehicle = z.infer<typeof VehicleZodSchema>;
