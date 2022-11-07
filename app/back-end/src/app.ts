6df5afe1607c0b796b55dfdadb1f2711bf45d1af
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-06 21:45:47 -0300

    feat(dev-back-end-user-auth): add the user routes to the app

--- app/back-end/src/app.ts
+++ app/back-end/src/app.ts
@@ -4,12 +4,14 @@ import helmet from 'helmet';
 import 'express-async-errors';
 import errorHandler from './middleware/error';
 import vehicleRouter from './routes/Vehicle';
+import userRouter from './routes/User';
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(vehicleRouter);
+app.use(userRouter);
 app.use(errorHandler);
 
 export default app;
