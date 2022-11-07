44283e448ae735a6788cd4a6dd8848a25de8c202
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-07 15:44:35 -0300

    feat(dev-seeder-mongodb): create an initial database for vehicles
    it will be used by the seeder

---
+++ app/seeder/src/data/vehicles.js
@@ -0,0 +1,24 @@
+const vehiclesData = [
+  {
+    model: 'Uno',
+    brand: 'Fiat',
+    description: 'Praesent tincidunt ut nisi at placerat. Sed ultricies accumsan purus non rutrum. Suspendisse eleifend laoreet neque ornare volutpat. Phasellus nec tellus non dui porta faucibus ac ac erat. Fusce non.',
+    year: 2010,
+    color: 'vermelho',
+    buyValue: 15600,
+    image: 'url',
+  },
+  {
+    model: 'Gol',
+    brand: 'Volkswagen',
+    description: 'Praesent tincidunt ut nisi at placerat. Sed ultricies accumsan purus non rutrum. Suspendisse eleifend laoreet neque ornare volutpat. Phasellus nec tellus non dui porta faucibus ac ac erat. Fusce non.',
+    year: 2012,
+    color: 'branco',
+    buyValue: 13600,
+    image: 'url',
+  },
+];
+
+module.exports = {
+  vehiclesData,
+};
