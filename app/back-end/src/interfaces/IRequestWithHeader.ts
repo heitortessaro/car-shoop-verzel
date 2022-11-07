f706e37a8b4c9dc8f2f89fdceba93fff025abcd5
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-07 07:41:51 -0300

    fix(dev-back-end-user-auth): change IRequestWithHeader interface name

--- app/back-end/src/interfaces/IRequestWithHeader.ts
+++ app/back-end/src/interfaces/IRequestWithHeader.ts
@@ -3,7 +3,7 @@ import { Request } from 'express';
 
 // explicação: https://javascript.plainenglish.io/how-to-create-custom-headers-with-express-and-typescript-b964d3f0be89
 
-export default interface IRequestAuthorization extends Request {
+export default interface IRequestWithHeader extends Request {
   headers: IncomingHttpHeaders & {
     authorization?: string,
   }
