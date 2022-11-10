8b812e7bffb56f4333d195c6b8225af88b7892a3
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-09 22:37:39 -0300

    feat(dev-front-end): add the browserRouter to the react application

--- app/front-end/src/main.jsx
+++ app/front-end/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './features/store/store';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
