d909e51b91d6f2dd569c43de13a7bdc0e70c6c01
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-10 15:43:23 -0300

    feat(dev-front-end): create vehicle card component
    it will show the information about each vehicle in sale

---
+++ app/front-end/src/components/cardVehicle/CardVehicle.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function CardVehicle({ model, brand, description, year, buyValue, image }) {
+  return (
+    <div className="card w-96 bg-base-100 text-zinc-700 shadow-xl">
+      <figure className="px-4 pt-4">
+        <img src={image} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {model}
+          <div className="badge badge-secondary">{` Valor: R$ ${buyValue}`}</div>
+        </h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{brand}</div>
+          <div className="badge badge-outline">{year}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+CardVehicle.propTypes = {
+  model: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  buyValue: PropTypes.number.isRequired
+};
