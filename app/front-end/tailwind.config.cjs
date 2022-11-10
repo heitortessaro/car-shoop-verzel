29039e80819b76da3feb81234cf6e17bdf3f67d4
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-10 14:30:57 -0300

    chore(dev-front-end): updates tailwind configuration

--- app/front-end/tailwind.config.cjs
+++ app/front-end/tailwind.config.cjs
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')]
 };
