a32c2d89e9e30e51098c39be673faf6aa6a007b6
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-09 21:44:54 -0300

    ci(dev-front-end): add and configure eslint and pretier

--- app/front-end/src/App.jsx
+++ app/front-end/src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-      <button class="btn">Button</button>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <button className="btn">Button</button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -32,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
