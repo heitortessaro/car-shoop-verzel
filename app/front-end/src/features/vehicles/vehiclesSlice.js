7004d7e916d5d95beb23f9d7c0cedcecb36d5332
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-09 22:30:39 -0300

    feat(dev-front-end): create vehiclesSlice to store states related to vehicles
    uses the getVehicles Tunk

---
+++ app/front-end/src/features/vehicles/vehiclesSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getVehicles } from './getvehicles';
+
+const initialState = {
+  vehicles: [],
+  loading: false,
+  requestError: false
+};
+
+const vehiclesSlice = createSlice({
+  name: 'vehicles',
+  initialState,
+  reducers: {
+    resetRequestError: (state) => {
+      state.requestError = false;
+    }
+  },
+  extraReducers: {
+    [getVehicles.pending]: (state) => {
+      state.loading = true;
+    },
+    [getVehicles.fulfilled]: (state, { payload }) => {
+      state.vehicles = payload.results;
+      state.loading = false;
+    },
+    [getVehicles.rejected]: (state) => {
+      state.loading = false;
+      state.requestError = true;
+    }
+  }
+});
+
+export const { resetRequestError } = vehiclesSlice.actions;
+
+export default vehiclesSlice;
