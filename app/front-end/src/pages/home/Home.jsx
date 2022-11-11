66282dc7a3a1abd336168aacd5d4bd9843c32aea
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-10 22:36:23 -0300

    feat(dev-front-end): Add the Home page
    it will contain the cardvehicles

---
+++ app/front-end/src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CardVehicle from '../../components/cardVehicle/CardVehicle';
+import getVehicles from '../../features/vehicles/getvehicles';
+import { selectVehicles, selectLoading } from '../../features/vehicles/vehiclesSlice';
+
+export default function Home() {
+  const vehicleList = useSelector(selectVehicles);
+  const loading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getVehicles());
+  }, []);
+
+  console.log(vehicleList);
+
+  return (
+    <main className="w-screen grow bg-slate-200 text-slate-800">
+      {loading && (
+        <div className="fixed bottom-24 right-0 h-full w-full flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      )}
+      {!loading && vehicleList.map((e, i) => <CardVehicle key={i} vehicleInfo={vehicleList[i]} />)}
+    </main>
+  );
+}
