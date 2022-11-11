b578f1f5a726afc5940f4ac3a9f0562e67afe483
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-10 22:35:24 -0300

    fix(dev-front-end): update the CardVehicle to receive one prop only

--- app/front-end/src/components/cardVehicle/CardVehicle.jsx
+++ app/front-end/src/components/cardVehicle/CardVehicle.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function CardVehicle({ model, brand, description, year, buyValue, image }) {
+export default function CardVehicle({ vehicleInfo }) {
+  console.log('chegou');
+  const { model, brand, description, year, buyValue, image } = vehicleInfo;
+  const imageURL = `${import.meta.env.VITE_API_BASE_URL}${image}`;
   return (
     <div className="card w-96 bg-base-100 text-zinc-700 shadow-xl">
       <figure className="px-4 pt-4">
-        <img src={image} alt="Shoes" className="rounded-xl" />
+        <img src={imageURL} alt={`Image from ${model} in sale`} className="rounded-xl" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
@@ -26,10 +29,12 @@ export default function CardVehicle({ model, brand, description, year, buyValue,
 }
 
 CardVehicle.propTypes = {
-  model: PropTypes.string.isRequired,
-  brand: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
-  buyValue: PropTypes.number.isRequired
+  vehicleInfo: PropTypes.shape({
+    model: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    buyValue: PropTypes.number.isRequired
+  })
 };
