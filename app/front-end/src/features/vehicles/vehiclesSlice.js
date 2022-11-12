fe4372252d3fdc5f3add7159313f8ab8ac388dc1
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-11 22:06:20 -0300

    feat(dev-front-end): create sortVehicleList action inside vehicle list

--- app/front-end/src/features/vehicles/vehiclesSlice.js
+++ app/front-end/src/features/vehicles/vehiclesSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import getVehicles from './getvehicles';
 
 const initialState = {
+  receivedVehicles: [],
   vehiclesList: [],
   loading: true,
   requestError: false
@@ -13,6 +14,28 @@ const vehiclesSlice = createSlice({
   reducers: {
     resetRequestError: (state) => {
       state.requestError = false;
+    },
+    sortVehicleList: (state, { payload }) => {
+      console.log(payload);
+      switch (payload) {
+        case 'anoCrescente':
+          state.vehiclesList = state.receivedVehicles.sort((a, b) => a.year > b.year);
+          break;
+        case 'anoDescrecente':
+          state.vehiclesList = state.receivedVehicles.sort((a, b) => a.year < b.year);
+          break;
+        case 'valorCrecente':
+          state.vehiclesList = state.receivedVehicles.sort((a, b) => a.buyValue > b.buyValue);
+          break;
+        case 'valorDecrecente':
+          state.vehiclesList = state.receivedVehicles.sort((a, b) => a.buyValue < b.buyValue);
+          break;
+        case 'modeloAlfabetico':
+          state.vehiclesList = state.receivedVehicles.sort((a, b) => a.model > b.model);
+          break;
+        default:
+          break;
+      }
     }
   },
   extraReducers: {
@@ -20,7 +43,8 @@ const vehiclesSlice = createSlice({
       state.loading = true;
     },
     [getVehicles.fulfilled]: (state, { payload }) => {
-      state.vehiclesList = payload;
+      state.receivedVehicles = payload;
+      state.vehiclesList = payload.sort((a, b) => a.buyValue < b.buyValue);
       state.loading = false;
     },
     [getVehicles.rejected]: (state) => {
@@ -33,6 +57,6 @@ const vehiclesSlice = createSlice({
 export const selectVehicles = (state) => state.vehicles.vehiclesList;
 export const selectLoading = (state) => state.vehicles.loading;
 
-export const { resetRequestError } = vehiclesSlice.actions;
+export const { resetRequestError, sortVehicleList } = vehiclesSlice.actions;
 
 export default vehiclesSlice.reducer;
