bd5d67c8a47786be3100b2ed3ac6ede9accf5378
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-14 20:54:05 -0300

    feat(dev-front-end): add the control form to the admin route

--- app/front-end/src/App.jsx
+++ app/front-end/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import './App.css';
+import ControlForm from './pages/controlForm/ControlForm';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<ControlForm />} />
       </Routes>
       <Footer />
     </div>
