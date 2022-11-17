dec1033a64e985b9953b09c5211d0006f66df663
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-17 19:20:10 -0300

    fix(dev-front-end-admin-panel): add the vehicle id to the api url when updating it

--- app/front-end/src/features/vehicles/actions/updateVehicle.js
+++ app/front-end/src/features/vehicles/actions/updateVehicle.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 const updateVehicle = createAsyncThunk(
   'vehicles/updateVehicle',
-  async ({ authorizationToken, formData }) => {
-    let url = `${import.meta.env.VITE_API_BASE_URL}/vehicles`;
+  async ({ authorizationToken, formData, vehicleId }) => {
+    let url = `${import.meta.env.VITE_API_BASE_URL}/vehicles/${vehicleId}`;
     try {
       const { data } = await axios({
         method: 'put',
