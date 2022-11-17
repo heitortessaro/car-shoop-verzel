cf2d55de0b8dc3caad7008dcef23896a0bbb4743
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-11-16 21:47:49 -0300

    feat(dev-front-end-admin-panel): include the DeleteConfirmationModal to Home.
    it shows the componente based on the state ShowModal from vehicleSlice.

--- app/front-end/src/pages/home/Home.jsx
+++ app/front-end/src/pages/home/Home.jsx
@@ -2,17 +2,25 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CardVehicle from '../../components/cardVehicle/CardVehicle';
 import SelectBar from '../../components/selectBar/SelectBar';
+import DeleteConfirmationModal from '../../components/deleteConfirmationModal/DeleteConfirmationModal';
 import getVehicles from '../../features/vehicles/actions/getvehicles';
-import { selectVehicles, selectLoading } from '../../features/vehicles/vehiclesSlice';
+import {
+  selectVehicles,
+  selectLoading,
+  selectShowModal
+} from '../../features/vehicles/vehiclesSlice';
 
 export default function Home() {
   const vehicleList = useSelector(selectVehicles);
   const loading = useSelector(selectLoading);
+  const showModal = useSelector(selectShowModal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getVehicles());
   }, []);
+
+  useEffect(() => {}, [showModal]);
 
   return (
     <main className="w-screen grow bg-slate-200 text-slate-800 flex flex-wrap	gap-4 justify-center pb-4">
@@ -22,7 +30,8 @@ export default function Home() {
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
         </div>
       )}
-      {!loading && vehicleList.map((e, i) => <CardVehicle key={i} vehicleInfo={vehicleList[i]} />)}
+      {!loading && vehicleList.map((vehicle, i) => <CardVehicle key={i} vehicleInfo={vehicle} />)}
+      {showModal && <DeleteConfirmationModal />}
     </main>
   );
 }
